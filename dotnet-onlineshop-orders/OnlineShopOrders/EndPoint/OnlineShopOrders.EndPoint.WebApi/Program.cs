ecfda773b951248e08bdf66f6e79377a09a28cf4
javidderakhshanee <149783685+javidderakhshanee@users.noreply.github.com>
2023-11-05 21:53:21 +0330

    add healthCheck

--- dotnet-onlineshop-orders/OnlineShopOrders/EndPoint/OnlineShopOrders.EndPoint.WebApi/Program.cs
+++ dotnet-onlineshop-orders/OnlineShopOrders/EndPoint/OnlineShopOrders.EndPoint.WebApi/Program.cs
@@ -3,10 +3,14 @@ using OnlineShopOrders.Infrastructure.Configuration;
 using OnlineShopOrders.Core.ApplicationService;
 using OnlineShopOrders.Infrastructure.Persistence;
 using OnlineShopOrders.Logger;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services
+builder.Services.AddHealthChecks();
+
+builder.Services       
        .AddCustomLogger()
        .AddApplicationService()
        .AddPersistence();
@@ -35,5 +39,14 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.MapHealthChecks("/hc", new HealthCheckOptions
+{
+    ResultStatusCodes =
+    {
+        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+        [HealthStatus.Degraded] = StatusCodes.Status200OK,
+        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+    }
+});
 
 app.Run();
